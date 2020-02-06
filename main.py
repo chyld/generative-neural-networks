a9aec24c8256cdc25e07610eb1886973d77e8101
Chyld Medford <chyld.medford@gmail.com>
2020-02-05 22:35:05 -0600

    dense, conv, autoencoder

---
+++ main.py
@@ -0,0 +1,56 @@
+from functools import reduce
+from operator import mul
+
+from tensorflow.keras.datasets.mnist import load_data
+from tensorflow.keras.layers import Input, Dense, LeakyReLU, Flatten, Conv2D, MaxPooling2D, Dropout, Reshape
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+def eda():
+    (x_train, y_train), (x_test, y_test) = load_data()
+    x_train = x_train.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+
+    x_train = x_train.reshape(-1, 28, 28, 1)
+    x_test = x_test.reshape(-1, 28, 28, 1)
+
+    return x_train, x_test, y_train, y_test
+
+def dense_encoder(shape, neurons, dim):
+    i = Input(shape=shape)
+    x = Flatten()(i)
+
+    x = Dense(units=neurons)(x)
+    x = LeakyReLU()(x)
+
+    x = Dense(units=dim)(x)
+    x = LeakyReLU()(x)
+
+    return Model(i, x, name='Dense-Encocder')
+
+def dense_decoder(shape, neurons, dim):
+    i = Input(shape=dim)
+
+    x = Dense(units=neurons)(i)
+    x = LeakyReLU()(x)
+
+    flattened = reduce(mul, shape)
+    x = Dense(units=flattened)(x)
+    x = LeakyReLU()(x)
+
+    x = Reshape(shape)(x)
+
+    return Model(i, x, name='Dense-Decoder')
+
+def conv_encoder(shape, neurons, dim):
+    i = Input(shape=shape)
+    x = Conv2D(32, padding='same', kernel_size=(3,3), activation='relu')(i)
+    x = MaxPooling2D(pool_size=(2,2))(x)
+    x = Conv2D(64, padding='same', kernel_size=(3,3), activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2,2))(x)
+    x = Flatten()(x)
+    x = Dense(units=neurons)(x)
+    x = LeakyReLU()(x)
+    x = Dense(units=dim)(x)
+    x = LeakyReLU()(x)
+    return Model(i, x, name='Conv-Encoder')
